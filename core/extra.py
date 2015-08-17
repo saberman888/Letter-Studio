3f8c255b792273728bdba4f57f24be0cd46db13d
saberman888 <saberman8881@gmail.com>
2015-08-17 17:07:45 -0600

    extra.py
    wxprompt function finished.

--- core/extra.py
+++ core/extra.py
@@ -4,4 +4,6 @@ def wxprompt(parent=None, message='', default_value=''):
     dialog = wx.TextEntryDialog(parent, message, defaultValue=default_value)
     dialog.ShowModal()
     result = dialog.GetValue()
+    dialog.Destroy()
     return result
+
