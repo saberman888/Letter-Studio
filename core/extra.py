d47f2d66296728a4b219dfdb514660165f12a598
saberman888 <saberman8881@gmail.com>
2015-08-24 20:01:57 -0600

    extra.py
    no longer needed.

--- core/extra.py
+++
@@ -1,9 +0,0 @@
-import wx
-
-def wxprompt(parent=None, message='', default_value=''):
-    dialog = wx.TextEntryDialog(parent, message, defaultValue=default_value)
-    dialog.ShowModal()
-    result = dialog.GetValue()
-    dialog.Destroy()
-    return result
-
