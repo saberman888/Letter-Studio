6be554331a2a7833bcfb2046a5340aa1ae0f8073
saberman888 <saberman8881@gmail.com>
2015-08-25 12:25:53 -0600

    ctools.py
    Just a name change.

--- core/ctools.py
+++ core/ctools.py
@@ -32,7 +32,7 @@ def delete_words(conlang, mode):
             wx.MessageBox("There are no words to delete", wx.OK)
         return 0
 
-def delete_conlang(conlang):
+def dump_conlang(conlang):
     delete_dialects(conlang, 1)
     delete_words(conlang, 1)
     del conlang
