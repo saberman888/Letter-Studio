b77860eda6e4f246864e894608e6294a43de4f1c
saberman888 <saberman8881@gmail.com>
2015-08-25 16:48:16 -0600

    launch.py
    Added to the launcher a option to update by using -u command to update.

--- launcher/launch.py
+++ launcher/launch.py
@@ -1,22 +1,33 @@
 import sys
 import os
 def main():
-    try:
-        sys.path.insert(0, 'core.zip')
-        from core.conassets import *
-        from core.conassets import *
-        from core.ctools import *
-        #Todo
-    except IOError:
-        print "Core not found."
-        print "Downloading core..."
-        import urllib
-        response = urllib.urlretrieve('null', 'core.zip')
-        print "Done."
-        print "Press enter to continue."
-        i = raw_input()
-        os.system('cls')
-        main()
+    if sys.argv[1] != '-u': #-u stands for update.
+        
+        try:
+            sys.path.insert(0, 'core.zip')
+            from core.conassets import *
+            from core.conassets import *
+            from core.ctools import *
+            #Todo
+        except IOError:
+            print "Core not found."
+            print "Downloading core..."
+            import urllib
+            response = urllib.urlretrieve(url, 'core.zip')
+            print "Done."
+            print "Press enter to continue."
+            i = raw_input()
+            os.system('cls')
+            main()
+    elif sys.argv[1] == '-u':
+        print "Updating..."
+        try:
+            import urlib
+            response = urllib.urlretrieve(url, 'core.zip')
+            print "Done!"
+            print "Press enter to exit."
+            i = raw_input()
+            sys.exit(0)
 
 
 if __name__ == "__main__":
