b22d951f3266078fa90eca41c515685b665f8d78
saberman888 <saberman8881@gmail.com>
2015-12-28 18:13:35 -0700

    UTF-16 support

--- core/language.py
+++ core/language.py
@@ -34,7 +34,7 @@ def load_language(configurate_file_name):
     with open(configurate_file_name, 'r') as cfl:
         for line in cfl.readlines():
             linez = line.split("=")
-            Dictionary[linez[0]] = unicode(u'%s' % linez[1])
+            Dictionary[linez[0].decode("utf16")] = linez[1].decode("utf16"))
     cfl.close()
 
 
