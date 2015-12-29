a4a7613b35ff9bb69dd7a1e9d90a05b60e579be5
saberman888 <saberman8881@gmail.com>
2015-12-28 18:10:59 -0700

    UTF-16 support

--- core/cio.py
+++ core/cio.py
@@ -20,28 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# The MIT License (MIT)
-#
-# Copyright (c) 2015 saberman888
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 from wx.alert import *
 from conassets import *
 from ctools import *
@@ -54,29 +32,29 @@ def save(filename, conlang):
         with open(filename, 'w') as k:
             # Start with language's metadata
             k.write("!METADATA_START\n")
-            k.write("\t-->!CONLANG:%s:\n".encode("utf8") % conlang.Name)
-            k.write("\t-->!AUTHOR:%s:\n".encode("utf8") % conlang.Author)
-            k.write("\t-->!AUTHOR:%s:\n".encode("utf8") % conlang.Family)
-            k.write("\t-->!Typology:%s:\n".encode("utf8") % conlang.T_Type)
-            k.write("\t-->!Alignment:%s:\n".encode("utf8") % conlang.A_Type)
-            k.write("\t-->!Language-Type:%s:\n".encode("utf8") % conlang.L_Type)
+            k.write("\t-->!CONLANG:%s:\n".encode("utf16") % conlang.Name.encode("utf16"))
+            k.write("\t-->!AUTHOR:%s:\n".encode("utf16") % conlang.Author.encode("utf16")
+            k.write("\t-->!AUTHOR:%s:\n".encode("utf16") % conlang.Family.encode("utf16"))
+            k.write("\t-->!Typology:%s:\n".encode("utf16") % conlang.T_Type.encode("utf16"))
+            k.write("\t-->!Alignment:%s:\n".encode("utf16") % conlang.A_Type.encode("utf16"))
+            k.write("\t-->!Language-Type:%s:\n".encode("utf16") % conlang.L_Type.encode("utf16"))
             k.write("!METADATA_END\n")
 
             k.write("!DIALECTS_START\n")
             for dialect in conlang.dialects:
-                k.write("\t-->!DIALECT:%s:%s:\n".encode("utf8") % (dialect.Name, Format4Save(dialect.description)))
+                k.write("\t-->!DIALECT:%s:%s:\n".encode("utf16") % (dialect.Name, Format4Save(dialect.description)))
             k.write("!DIALECTS_END\n")
 
             k.write("!CLASSES_START\n")
             for CLASS in conlang.classes:
-                k.write("\t-->!CLASS:%s:%s:\n".encode("utf8") % (CLASS.Name, Format4Save(CLASS.description)))
+                k.write("\t-->!CLASS:%s:%s:\n".encode("utf16") % (CLASS.Name.encode("utf16"), Format4Save(CLASS.description).encode("utf16")))
             k.write("!CLASSES_END\n")
 
             k.write("!DICTIONARY_START\n")
             for word in conlang.words:
-                k.write("\t-->!word:%s:%s:%s:%s:%s:%s:%s:%s:\n".encode("utf8") % (
-                    word.word, word.definition, word.pos, word.register,
-                    word.Class, word.dialect, word.source_lang, word.notes))
+                k.write("\t-->!word:%s:%s:%s:%s:%s:%s:%s:%s:\n".encode("utf16") % (
+                    word.word.encode("utf16"), word.definition.encode("utf16"), word.pos, word.register.endcode("utf16"),
+                    word.Class.encode("utf16"), word.dialect.encode("utf16"), word.source_lang.encode("utf16"), word.notes.encode("utf16")))
             k.write("!DICTIONARY_END\n")
 
             k.close()
@@ -117,15 +95,15 @@ def load(filename):
                         meta.append(mline[1])
                     elif cmode == 2:
                         dline = v.split(":")
-                        new_word = Word(dline[1], dline[2], dline[3], dline[4], dline[5], dline[6], dline[7], dline[8])
+                        new_word = Word(dline[1].decode("utf16"), dline[2].decode("utf16"), dline[3].decode("utf16"), dline[4].decode("utf16"), dline[5].decode("utf16"), dline[6].decode("utf16"), dline[7].decode("utf16"), dline[8].decode("utf16"))
                         dict_.append(new_word)
                     elif cmode == 3:
                         diline = v.split(":")
-                        new_dialect = Dialect(diline[1], diline[2])
+                        new_dialect = Dialect(diline[1].decode("utf16"), diline[2].decode("utf16"))
                         dialects.append(new_dialect)
                     elif cmode == 4:
                         cline = v.split(":")
-                        new_class = Class(cline[1], cline[2])
+                        new_class = Class(cline[1].decode("utf16"), cline[2].decode("utf16"))
                         classes.append(new_class)
 
                 elif v.endswith("START\n"):
@@ -147,9 +125,9 @@ def load(filename):
         #print "dialect total: %s \n" % len(dialects)
         #print "class total: %s\n" % len(classes)
         # Assemble the conlang data
-        new_conlang = Conlang(meta[0].decode("utf8"), meta[1].decode("utf8"), meta[2].decode("utf8"),
-                              meta[3].decode("utf8"),
-                              meta[4].decode("utf8"), meta[5].decode("utf8"))
+        new_conlang = Conlang(meta[0].decode("utf16"), meta[1].decode("utf16"), meta[2].decode("utf16"),
+                              meta[3].decode("utf16"),
+                              meta[4].decode("utf16"), meta[5].decode("utf16"))
         # Add all the assets to the conlang
         for word in dict_:
             word.add2list(new_conlang)
